59fcff6fbcc6606fbc4f3e0937e9e073bc239b62
fulin <13103779727@163.com>
2019-12-08 17:00:10 +0800

    fix:  修复velocity判断的问题

--- src/main/java/com/fulinlin/constant/GitCommitConstants.java
+++ src/main/java/com/fulinlin/constant/GitCommitConstants.java
@@ -6,12 +6,12 @@ package com.fulinlin.constant;
  * @create: 2019-12-08 11:37
  **/
 public class GitCommitConstants {
-    public static final String DEFAULT_TEMPLATE = "#if($type)${type}#end#if(${scope})(${scope})#end: #if(${subject})${subject}#end\n" +
+    public static final String DEFAULT_TEMPLATE = "#if($type)${type}#end#if($scope)(${scope})#end: #if($subject)${subject}#end\n" +
             "${newline}\n" +
-            "#if(${body})${body}#end\n" +
+            "#if($body)${body}#end\n" +
             "${newline}\n" +
-            "#if(${changes})BREAKING CHANGE: ${changes}#end\n" +
+            "#if($changes)BREAKING CHANGE: ${changes}#end\n" +
             "${newline}\n" +
-            "#if(${closes})Closes ${closes}#end\n";
+            "#if($closes)Closes ${closes}#end\n";
 
 }
