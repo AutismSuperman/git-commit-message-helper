57bef35008d113e949ebad9659fbc403e24ab18d
FuLin <13103779727@163.com>
2020-05-19 20:14:17 +0800

    fix(template): change default template
    - ${newline} inside if statements
    
    Closes #4

--- src/main/java/com/fulinlin/constant/GitCommitConstants.java
+++ src/main/java/com/fulinlin/constant/GitCommitConstants.java
@@ -6,8 +6,8 @@ package com.fulinlin.constant;
  * @create: 2019-12-08 11:37
  **/
 public class GitCommitConstants {
-    public static final String DEFAULT_TEMPLATE = "#if($type)${type}#end#if($scope)(${scope})#end: #if($subject)${subject}${newline}#end\n" +
-            "#if($body)${body}${newline}#end\n" +
-            "#if($changes)BREAKING CHANGE: ${changes}${newline}#end\n" +
-            "#if($closes)Closes ${closes}#end\n";
+    public static final String DEFAULT_TEMPLATE = "#if($type)${type}#end#if($scope)(${scope})#end: #if($subject)${subject}#end\n" +
+            "#if($body)${newline}${newline}${body}#end\n" +
+            "#if($changes)${newline}${newline}BREAKING CHANGE: ${changes}#end\n" +
+            "#if($closes)${newline}${newline}Closes ${closes}#end\n";
 }
