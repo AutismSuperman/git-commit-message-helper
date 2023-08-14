28dae1d9c497786e1d2ab2793885c9d41e25d56e
ghost-destroyer <xuehu622826@126.com>
2023-08-14 11:08:19 +0800

    perf(plugin.xml): 修改相关插件信息

--- src/main/java/icons/PluginIcons.java
+++ src/main/java/icons/PluginIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.Icon;
 
 public interface PluginIcons {
-    Icon ICON = IconLoader.getIcon("/icons/icon.png");
+    Icon ICON = IconLoader.findIcon("/icons/icon.png");
 }
